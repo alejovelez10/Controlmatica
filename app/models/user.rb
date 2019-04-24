5e02006ad6fe7cb8c020c2c4cf68c45554b88d52
alejovelez10 <alejovelez10@gmail.com>
2019-04-24 13:14:34 -0500

    edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
     mount_uploader :avatar, AvatarUploader
-    belongs_to :rol
+    belongs_to :rol, optional: true
     
 end
