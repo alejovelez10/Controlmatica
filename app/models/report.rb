0084a465ad618a811a949976c56844b8a5bb0b60
alejovelez10 <alejovelez10@gmail.com>
2019-12-05 16:45:45 -0500

    organizando code

--- app/models/report.rb
+++ app/models/report.rb
@@ -63,7 +63,6 @@ class Report < ApplicationRecord
       count = Report.where(customer_id: self.customer_id).maximum(:count)
       self.count = count == 0 || count.blank? || count.nil? ? 1 : count + 1
       customer_prefix = Customer.find(self.cost_center.customer.id).code
-      self.report_code = count == 0 || count.blank? || count.nil? ? 1 : count
       self.code_report = "REP-" + customer_prefix + "-" + self.report_code.to_s + "-" + Time.now.year.to_s
     end
   end
@@ -90,7 +89,6 @@ class Report < ApplicationRecord
       count = Report.where(customer_id: self.customer_id).maximum(:count)
       self.count = count == 0 || count.blank? || count.nil? ? 1 : count + 1
       customer_prefix = Customer.find(costcenter.customer.id).code
-      self.report_code = count == 0 || count.blank? || count.nil? ? 1 : count + 1
       self.code_report = "REP-" + customer_prefix + "-" + self.report_code.to_s + "-" + Time.now.year.to_s
     
     else
