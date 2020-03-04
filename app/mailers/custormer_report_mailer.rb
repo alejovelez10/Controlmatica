eee774331d30282dfff50bf76c73c0b7f3bcd5d3
alejovelez10 <alejovelez10@gmail.com>
2020-03-03 21:25:14 -0500

    organizando email

--- app/mailers/custormer_report_mailer.rb
+++ app/mailers/custormer_report_mailer.rb
@@ -1,7 +1,7 @@
 class CustormerReportMailer < ApplicationMailer
   def approval_customer_email(customer_report)
     @customer_report = customer_report
-    mail(to: "aprobaciones@controlmatica.com.co", :from => @customer_report.contact.email, subject: "El reporte #{customer_report.report_code} fue aprobado")
+    mail(to: "alejovelez10@gmail.com", :from => @customer_report.contact.email, subject: "El reporte #{customer_report.report_code} fue aprobado")
   end
 
   def approval_email(customer_report)
