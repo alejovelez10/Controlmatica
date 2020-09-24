e9c6f71133bd07652bff73912e8cfdca6a66eb87
alejovelez10 <alejovelez10@gmail.com>
2020-09-24 11:38:24 -0500

    organizando pdf

--- app/controllers/employed_performance_controller.rb
+++ app/controllers/employed_performance_controller.rb
@@ -1,5 +1,5 @@
 class EmployedPerformanceController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def show
     informe = ModuleControl.find_by_name("Informe de rendimiento")
@@ -8,32 +8,28 @@ class EmployedPerformanceController < ApplicationController
   end
 
   def info_pdf
-  	@user = User.find(params[:user_id])
-  	@fecha_desde = params[:fecha_desde]
-  	@fecha_hasta = params[:fecha_hasta]
-  	@reports = Report.where(report_execute: @user).where("report_date >= ?" ,@fecha_desde).where("report_date <= ?" , @fecha_hasta).order(report_date: :asc)
+    @user = User.find(params[:user_id])
+    @fecha_desde = params[:fecha_desde]
+    @fecha_hasta = params[:fecha_hasta]
+    @reports = Report.where(report_execute: @user).where("report_date >= ?", @fecha_desde).where("report_date <= ?", @fecha_hasta).order(report_date: :asc)
 
-
-
-  	respond_to do |format|
-        #format.html
-        format.pdf do
-          render :pdf => "formatos1",
-          :template => 'employed_performance/show_pdf.pdf.erb',
-          :layout => 'pdf.html.erb',
-          :show_as_html => params[:debug].present?
+    respond_to do |format|
+      #format.html
+      format.pdf do
+        render :pdf => "formatos1",
+               :template => "employed_performance/show_pdf.pdf.erb",
+               :layout => "pdf.html.erb",
+               :show_as_html => params[:debug].present?
       end
-    end 
-
-  end	
+    end
+  end
 
   def info_pdf_new
     array = []
     user = User.find(params[:user_id])
-  	fecha_desde = params[:fecha_desde]
-  	fecha_hasta = params[:fecha_hasta]
-  	reports = Report.where(report_execute: user).where("report_date >= ?", fecha_desde).where("report_date <= ?" , fecha_hasta).order(report_date: :asc)
-
+    fecha_desde = params[:fecha_desde]
+    fecha_hasta = params[:fecha_hasta]
+    reports = Report.where(report_execute: user).where("report_date >= ?", fecha_desde).where("report_date <= ?", fecha_hasta).order(report_date: :asc)
 
     reports.each do |report|
       array << [report.report_date.strftime("%d-%m-%Y"), report.working_time + report.displacement_hours, report.cost_center.code, report.customer.name, report.work_description, report.cost_center.description]
@@ -41,40 +37,37 @@ class EmployedPerformanceController < ApplicationController
 
     headers = ["FECHA", "HORAS", "CENTRO DE COSTO", "CLIENTE", "DESCRIPCION ACTIVIDAD", "DESCRIPCION CENTRO DE COSTO"]
 
-    data = [ 
-      ["", {:content => "REPORTE DE EFICIENCIA", :colspan => 2, :align => :center}, {:content => "", :align => :center}],
+    data = [
+      ["", { :content => "REPORTE DE EFICIENCIA", :colspan => 2, :align => :center }, { :content => "", :align => :center }],
     ]
 
-    dataTable1 = [ 
-      ["Empleado: #{user.names}", {:content => "Fecha hasta: #{fecha_hasta}", :rowspan => 2, :padding => [15, 0, 0, 10]}], 
-      ["Fecha Desde: #{fecha_desde}"], 
+    dataTable1 = [
+      [{ :content => "Empleado: #{user.names}", :colspan => 2 }],
+      ["Fecha Desde: #{fecha_desde}", "Fecha hasta: #{fecha_hasta}"],
     ]
 
     dataTable2 = [["Total: #{reports.sum(:working_time)}"]]
 
-
     content = "\n"
-    
 
     respond_to do |format|
       format.pdf do
         pdf = Prawn::Document.new
 
-        pdf.table(data, :width => 540, cell_style: { :padding => [20, 10, 20, 10]}) do |table|
+        pdf.table(data, :width => 540, cell_style: { :padding => [20, 10, 20, 10] }) do |table|
           table.rows(1..3).width = 72
         end
 
         pdf.text content
 
         pdf.table(dataTable1, :width => 540) do |table|
-
         end
 
         pdf.text content
 
-        pdf.table([headers] + array, :header => true, :width => 540, cell_style: {align: :center, :top_margin => 100} ) do |table|
+        pdf.table([headers] + array, :header => true, :width => 540, :column_widths => [38, 20, 9, 63, 98, 154], cell_style: { align: :center, :top_margin => 100, :size => 7 }) do |table|
           table.row(0).background_color = "2a3f54"
-          table.row(0).size = 9
+          table.row(0).size = 7
           table.row(0).text_color = "FFFFFF"
           table.rows(1..3).width = 72
         end
@@ -82,18 +75,13 @@ class EmployedPerformanceController < ApplicationController
         pdf.text content
 
         pdf.table(dataTable2, :width => 540) do |table|
-
         end
-
 
         send_data pdf.render,
           filename: "export.pdf",
-          type: 'application/pdf',
-          disposition: 'inline'
+          type: "application/pdf",
+          disposition: "inline"
       end
     end
   end
-  
-
-
 end
