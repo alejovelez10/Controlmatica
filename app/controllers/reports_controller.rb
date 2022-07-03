b4fae5eec6b698f7a1cf53285cb6f059a8504ec6
estivensm <salazarmoncadaestiven0@gmail.com>
2022-07-03 15:12:01 -0500

    arreglando el controlador de reportes, para que lleve la informacion necesaria

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -47,24 +47,24 @@ class ReportsController < ApplicationController
     if validate
       if params[:filtering] == "true"
         reports = Report.all.order(report_date: :desc).search(params[:work_description], params[:report_execute_id], params[:date_ejecution], params[:report_sate], params[:cost_center_id], params[:customer_id], params[:date_desde], params[:date_hasta], params[:code_report]).paginate(page: params[:page], :per_page => 10)
-        reports_total = Report.search(params[:work_description], params[:report_execute_id], params[:date_ejecution], params[:report_sate], params[:cost_center_id], params[:customer_id], params[:date_desde], params[:date_hasta], params[:code_report]).order(report_date: :desc)
+        reports_total = Report.search(params[:work_description], params[:report_execute_id], params[:date_ejecution], params[:report_sate], params[:cost_center_id], params[:customer_id], params[:date_desde], params[:date_hasta], params[:code_report]).count
       elsif params[:filtering] == "false"
         reports = Report.all.order(report_date: :desc).paginate(:page => params[:page], :per_page => 10)
-        reports_total = Report.all.order(report_date: :desc)
+        reports_total = Report.all.count
       else
         reports = Report.all.order(report_date: :desc).paginate(:page => params[:page], :per_page => 10)
-        reports_total = Report.all.order(report_date: :desc)
+        reports_total = Report.all.count
       end
     else
       if params[:filtering] == "true"
         reports = Report.where(report_execute_id: current_user.id).search(params[:work_description], params[:report_execute_id], params[:date_ejecution], params[:report_sate], params[:cost_center_id], params[:customer_id], params[:date_desde], params[:date_hasta], params[:code_report]).paginate(page: params[:page], :per_page => 10)
-        reports_total = Report.where(report_execute_id: current_user.id).search(params[:work_description], params[:report_execute_id], params[:date_ejecution], params[:report_sate], params[:cost_center_id], params[:customer_id], params[:date_desde], params[:date_hasta], params[:code_report])
+        reports_total = Report.where(report_execute_id: current_user.id).search(params[:work_description], params[:report_execute_id], params[:date_ejecution], params[:report_sate], params[:cost_center_id], params[:customer_id], params[:date_desde], params[:date_hasta], params[:code_report]).count
       elsif params[:filtering] == "false"
         reports = Report.where(report_execute_id: current_user.id).paginate(:page => params[:page], :per_page => 10)
-        reports_total = Report.where(report_execute_id: current_user.id)
+        reports_total = Report.where(report_execute_id: current_user.id).count
       else
         reports = Report.where(report_execute_id: current_user.id).paginate(:page => params[:page], :per_page => 10)
-        reports_total = Report.where(report_execute_id: current_user.id)
+        reports_total = Report.where(report_execute_id: current_user.id).count
       end
     end
 
--- app/javascript/components/Reports/index.jsx
+++ app/javascript/components/Reports/index.jsx
@@ -61,7 +61,7 @@ class index extends React.Component {
         .then(data => {
           this.setState({
             data: data.reports_paginate,
-            reports_total: data.reports_total.length,
+            reports_total: data.reports_total,
             exel_values: data.reports_total,
           });
         });
@@ -247,7 +247,7 @@ class index extends React.Component {
         .then(data => {
           this.setState({
             data: data.reports_paginate,
-            reports_total: data.reports_total.length,
+            reports_total: data.reports_total,
             exel_values: data.reports_total,
             activePage: 1
           });
@@ -264,7 +264,7 @@ class index extends React.Component {
       .then(data => {
         this.setState({
           data: data.reports_paginate,
-          reports_total: data.reports_total.length,
+          reports_total: data.reports_total,
           exel_values: data.reports_total,
           activePage: 1
         });
@@ -278,7 +278,7 @@ class index extends React.Component {
         .then(data => {
           this.setState({ 
             data: data.reports_paginate,
-            reports_total: data.reports_total.length,
+            reports_total: data.reports_total,
             exel_values: data.reports_total,
           });
         });
