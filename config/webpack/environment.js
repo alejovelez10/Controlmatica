44528ba073913b2cfbc33d166cd2be01fbfcfd0a
alejovelez10 <alejovelez10@gmail.com>
2026-03-09 23:35:47 -0500

    Enhance Webpack configuration for SweetAlert2 compatibility
    - Updated Babel loader settings to ensure SweetAlert2 is transpiled to ES5, addressing compatibility issues with UglifyJS.
    - Simplified Webpack production configuration by removing unnecessary TerserPlugin adjustments, streamlining the build process.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+// SweetAlert2 v11+ usa ES6 (const, let, arrow functions)
+// pero UglifyJS 2.x (de Webpacker 3.5) solo soporta ES5.
+// Forzamos que Babel transpile sweetalert2 a ES5.
+const babelLoader = environment.loaders.get('babel')
+if (babelLoader) {
+  babelLoader.exclude = function(modulePath) {
+    // No excluir sweetalert2 de Babel (transpilarlo)
+    if (/node_modules\/sweetalert2/.test(modulePath)) return false
+    // Excluir el resto de node_modules como siempre
+    return /node_modules/.test(modulePath)
+  }
+}
+
 module.exports = environment
